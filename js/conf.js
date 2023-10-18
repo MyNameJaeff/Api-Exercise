cb825541ed21a237ef156b586d70569437097066
MyNameJaeff <fredrik.andersson@elev.ga.ntig.se>
2023-10-18 12:34:55 +0200

    keys :(

---
+++ js/conf.js
@@ -0,0 +1,5 @@
+const codesKey = "38f8486d41a446d8b21f7dad7bc50b00";
+const departuresKey = "0ec3cd0e97da4f7aac66b93ab9f70240";
+const weatherKey = "11ce47de9c4fb72f072a0168191280dd";
+const bookApiKey = "fLvsHJKAaWQKCJQUzxiIVWisNVNQUuTL";
+const googleKey = "AIzaSyA2FKYjmb5x3D5EE8thpWHEtNJC-NAAeXc";
